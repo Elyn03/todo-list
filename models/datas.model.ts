434aa4d82b077620f12f624302e7318f9dc55628
Céline Eap <eapceline@gmail.com>
2024-03-01 21:46:17 +0100

    :sparkles: add interfaces

---
+++ models/datas.model.ts
@@ -0,0 +1,11 @@
+export interface Task {
+   title: string,
+   description: string | undefined,
+   date: string,
+   priority: string
+}
+
+export interface Category {
+   title: string,
+   description: string
+}
